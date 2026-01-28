c104de36c7d0257fa740af168fcc69674866e838
luisnavia <luisnavia26@gmail.com>
2026-01-28 15:10:36 -0500

    Verifier

---
+++ src/solver/Verifier.py
@@ -0,0 +1,57 @@
+def Verifier(MP, WP, matches):
+
+    #Turn all the inputs into dict
+    if isinstance(matches,dict):
+        MtoW = dict(matches)
+    else:
+        MtoW = {}
+        for m, w in matches:
+            if m not in MtoW:
+                MtoW[m] = w
+            else:
+                return "invalid"
+    men = set(MP.keys())
+    women = set(WP.keys())
+
+
+    for m,w in MtoW.items():
+        if w not in women:
+            return "invalid"
+        if m not in men:
+            return "invalid"
+
+    seenW = set()
+    for m, w in MtoW.items():
+        if w in seenW:
+            return "invalid"
+        seenW.add(w)
+
+    WtoM = {w: m for m, w in MtoW.items()}
+
+    Mranks = {
+        m: {w: i for i, w in enumerate(pref_list)}
+        for m, pref_list in men.items()
+    }
+
+    Wranks = {
+        w: {m: i for i, m in enumerate(pref_list)}
+        for w, pref_list in women.items()
+    }
+
+    #Checks Stability
+
+    for m in men:
+        if m not in MtoW:
+            continue
+        currentW = MtoW[m]
+
+        for w in men[m]:
+            if w == currentW:
+                break
+            wcurrentm = WtoM.get(w, None)
+            if wcurrentm is None:
+                return "unbalnced"
+            if Wranks[w][m] < Wranks[w][wcurrentm]:
+                return "unbalnced"
+    return "Valid"
+
